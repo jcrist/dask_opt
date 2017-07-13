834257eeb9d216a9ef680914a99f687b63186ff4
Thomas Greg Corcoran <thomasgregcorcoran@gmail.com>
2017-07-13 03:31:53 +0200

    Moved benchmarking code to separate repository

--- dask_searchcv/tests/test_async_model_selection.py
+++ dask_searchcv/tests/test_async_model_selection.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from dask.base import tokenize
 from distributed import Client
 from distributed.utils_test import loop, cluster
@@ -5,11 +7,15 @@ from sklearn.datasets import load_digits
 from sklearn.pipeline import Pipeline, FeatureUnion
 
 from dask_searchcv import GridSearchCV
-from dask_searchcv.async_model_selection import AsyncGridSearchCV
+from dask_searchcv.async_model_selection import AsyncGridSearchCV, CachingPlugin
 from dask_searchcv.utils_test import MockClassifier, ScalingTransformer
 import pandas as pd
 import numpy as np
 import pytest
+
+import logging
+
+log = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize(
@@ -82,3 +88,50 @@ def test_asyncgridsearchcv(model, param_grid, loop):
 
 def test_persisted_scores_are_kept():
     pass
+
+
+class MockScheduler(object):
+    def __init__(self):
+        self._keys = defaultdict(lambda: [])
+        self._plugins = []
+
+    def client_releases_keys(self, keys, client):
+        self._keys[client] = [k for k in self._keys[client] if k not in keys]
+
+    def client_desires_keys(self, keys, client):
+        self._keys[client] = self._keys[client] + keys
+
+    def add_plugin(self, plugin):
+        self._plugins.append(plugin)
+
+
+def test_cachingplugin():
+    logging.basicConfig(level=logging.DEBUG)
+
+    s = MockScheduler()
+
+    caching_plugin = CachingPlugin(s, cache_size=40)
+
+    s.add_plugin(caching_plugin)
+
+    caching_plugin.transition('a', start='processing', finish='memory', nbytes=30,
+                              startstops=[('compute', 0.1, 0.2)])
+
+    assert caching_plugin.total_bytes == 30
+    assert list(caching_plugin.scheduler._keys['fake-caching-client']) == ['a']
+
+    caching_plugin.transition(
+        'b', start='processing', finish='memory', nbytes=20,
+        startstops=[('compute', 0.1, 0.2)]
+    )
+
+    assert caching_plugin.total_bytes == 20
+    assert list(caching_plugin.scheduler._keys['fake-caching-client']) == ['b']
+
+    # Touching keys from the client
+    # -----------------------------
+    # Cannot affect key cost from client.gather since SchedulerPlugin doens't provide
+    #  that interface. Have to increment cost on the plugin via rpc from client.
+    #
+    # caching_plugin.touch('b')
+    # touch increases the score
