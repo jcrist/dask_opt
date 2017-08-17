1489546ff4062e031568cc9026925d24511b9517
Thomas Greg Corcoran <thomasgregcorcoran@gmail.com>
2017-08-17 13:36:55 +0200

    Scikit Learn 0.19 fixes

--- dask_searchcv/methods.py
+++ dask_searchcv/methods.py
@@ -14,7 +14,7 @@ from dask.base import normalize_token
 from sklearn.exceptions import FitFailedWarning
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.utils import safe_indexing
-from sklearn.utils.fixes import rankdata
+from sklearn.utils.stats import rankdata
 from sklearn.utils.validation import check_consistent_length, _is_arraylike
 
 from .utils import copy_estimator
