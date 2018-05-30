c09df4206a07f0bb6e1f81f72090e6147862a823
Scott Sievert <github@stsievert.com>
2018-05-29 21:45:28 -0500

    MAINT: mirrors the sklearn param API, uses model.score

--- dask_searchcv/adaptive.py
+++ dask_searchcv/adaptive.py
@@ -9,6 +9,7 @@ try:
 except:
     from sklearn.model_selection import train_test_split
 from sklearn import clone
+from sklearn.model_selection import ParameterSampler
 
 import distributed
 import dask.array as da
@@ -23,15 +24,19 @@ def _train(model, X, y, X_val, y_val, max_iter=1, classes=None, **kwargs):
     """
     s = kwargs.pop('s', 1)
     i = kwargs.pop('i', 1)
+    k = kwargs.pop('k', '_')
+    verbose = kwargs.pop('verbose', False)
     for iter in range(int(max_iter)):
-        print("s={s}, i={i}, iter={iter}".format(iter=iter, s=s, i=i))
+        if verbose:
+            msg = ("Training model {k} in bracket s={s} iteration {i}. "
+                   "This model is {percent:.1f}% trained for this bracket")
+            print(msg.format(iter=iter, k=k, s=s, i=i, percent=iter * 100.0 / max_iter))
         _ = model.partial_fit(X, y, classes)
-        model._iters += 1
 
-    y_hat = model.predict(X_val)
-    return {'val_loss': model.loss_fn(y_hat, y_val), 'model': model}
+    return model.score(X_val, y_val)
 
-def _bottom_k(choose_from, eval_with, k=1):
+
+def _top_k(choose_from, eval_with, k=1):
     assert k > 0, "k must be positive"
     assert set(choose_from.keys()) == set(eval_with.keys())
 
@@ -40,23 +45,79 @@ def _bottom_k(choose_from, eval_with, k=1):
     eval_with = [eval_with[key] for key in keys]
     idx = np.argsort(eval_with)
 
-    return {keys[i]: choose_from[keys[i]] for i in idx[:k]}
+    return {keys[i]: choose_from[keys[i]] for i in idx[-k:]}
 
 
-def _random_choice(params):
-    choice = {}
-    for k, v in params.items():
-        dist = stats.uniform(v['min'], v['max'] - v['min'])
-        r = dist.rvs()
-        if v.get('type', 'linear') == 'log':
-            value = 10**r
-        choice[k] = value
-    return choice
+def _random_choice(params, n=1):
+    configs = ParameterSampler(params, n)
+    return [{k: float(v) for k, v in config.items()}
+            for config in configs]
 
+
+def _successive_halving(params, model, n=None, r=None, s=None, verbose=False,
+                        shared=None, eta=3, _prefix=''):
+    client = distributed.get_client()
+    data = client.get_dataset('_dask_data')
+    best = {k: shared[k] for k in ['val_loss', 'model', 'config']}
+    classes = shared['classes'].get()
+
+    if any(x is None for x in [n, r, s]):
+        raise ValueError('n, r, and s are required')
+    ids = [_prefix + '-' + str(i) for i in range(n)]
+    params = _random_choice(params, n=len(ids))
+    params = {k: param for k, param in zip(ids, params)}
+    models = {}
+    final_val_losses = {k: np.inf for k in ids}
+    for k, param in params.items():
+        models[k] = clone(model).set_params(**param)
+
+    # TODO: manage memory with below
+    #  models = client.scatter(models)
+
+    history = []
+    iters = 0
+    for i in range(s + 1):
+        n_i = math.floor(n * eta**-i)
+        r_i = r * eta**i
+        iters += r_i
+        if verbose:
+            msg = ('Training {n} models for {r} iterations during iteration {i} '
+                   'of bracket={s}')
+            print(msg.format(n=n_i, r=r_i, i=i, s=s))
+
+        futures = {k: client.submit(_train, model, *data['train'],
+                                    *data['val'], max_iter=r_i, classes=classes,
+                                    s=s, i=i, k=k, verbose=verbose)
+                      for k, model in models.items()}
+        val_losses = client.gather(futures)
+        final_val_losses.update(val_losses)
+
+        models = _top_k(models, val_losses, k=max(1, math.floor(n_i / eta)))
+
+        best_ = {k: v.get() for k, v in best.items()}
+        if min(final_val_losses.values()) < best_['val_loss']:
+            min_loss_key = min(final_val_losses, key=final_val_losses.get)
+            best_['val_loss'] = val_losses[min_loss_key]
+            best_['config'] = params[min_loss_key]
+            #  best_['model'] = models[min_loss_key]
+            [best[k].set(v) for k, v in best_.items()]
+            if verbose:
+                print("New best val_loss={} found".format(best_['val_loss']))
+
+        history += [{'s': s, 'i': i, 'val_loss': val_losses[k], 'model_id': k,
+                     'iters': iters, 'best_val_loss': best_['val_loss'],
+                     **params[k]}
+                     for k, model, in models.items()]
+        if len(models) == 1:
+            break
+    return history
 
 class Hyperband:
     def __init__(self, params, model, iters, X, y, eta=3):
         """
+        An adaptive model selection method that trains models with
+        ``model.partial_fit`` and evaluates models with ``model.score``.
+
         Parameters
         ----------
         params :
@@ -77,7 +138,7 @@ class Hyperband:
         This implements the `Hyperband model selection algorithm`_
         and only requires one input, ``iters`` which is porprotional to
         "computational effort" required. Notably it does not require balancing
-        a trade-off between the number of configs to evaluate and how long to
+        a trade-off between the number of params to evaluate and how long to
         train each config.
 
         Hyperband has some theoritical guarantees on finding (close to) the
@@ -98,8 +159,6 @@ class Hyperband:
         self.classes = np.unique(y)
 
         n, d = X.shape
-        print(X.shape, y.shape)
-        print(X.dtype, y.dtype)
         # TODO: remove this
         if not isinstance(X, da.Array):
             raise ValueError('Hyperband currently only accepts data as dask arrays')
@@ -112,63 +171,6 @@ class Hyperband:
         self.history = []
         self._base_id = 0
 
-    def _successive_halving(self, params, n=None, r=None, s=None, verbose=False,
-                            shared=None, eta=3):
-        client = distributed.get_client()
-        data = client.get_dataset('_dask_data')
-        best = {k: shared[k] for k in ['val_loss', 'model', 'config']}
-        history = shared['history']
-
-        if any(x is None for x in [n, r, s]):
-            raise ValueError('n, r, and s are required')
-        ids = [self._base_id + i for i in range(n)]
-        self._base_id += len(ids)
-        configs = {k: _random_choice(self.params) for k in ids}
-        models = {}
-        final_val_losses = {k: np.inf for k in ids}
-        for k, config in configs.items():
-            models[k] = clone(self.model).set_params(**config)
-            models[k].loss_fn = sklearn.metrics.hinge_loss
-            models[k]._iters = 0
-
-        for i in range(s + 1):
-            n_i = math.floor(n * eta**-i)
-            r_i = r * eta**i
-            if verbose:
-                msg = ('Training {n} models for {r} iterations for the {i}th '
-                       'iteration of the {s}th bracket')
-                print(msg.format(n=n_i, r=r_i, i=i, s=s))
-
-            futures = {k: client.submit(_train, model, *data['train'],
-                                        *data['val'], max_iter=r_i,
-                                        classes=shared['classes'].get(), s=s, i=i)
-                          for k, model in models.items()}
-            results = client.gather(futures)
-            val_losses = {k: v['val_loss'] for k, v in results.items()}
-            models = {k: v['model'] for k, v in results.items()}
-            final_val_losses.update(val_losses)
-
-            models = _bottom_k(models, val_losses, k=max(1, math.floor(n_i / eta)))
-
-            best_ = {k: v.get() for k, v in best.items()}
-            if min(final_val_losses.values()) < best_['val_loss']:
-                min_loss_key = min(final_val_losses, key=final_val_losses.get)
-                best_['val_loss'] = val_losses[min_loss_key]
-                best_['config'] = configs[min_loss_key]
-                #  best_['model'] = models[min_loss_key]
-                [best[k].set(v) for k, v in best_.items()]
-                if verbose:
-                    print("New best val_loss={} found".format(best_['val_loss']))
-
-            history_ = history.get()
-            history_ += [{'s': s, 'i': i, 'val_loss': val_losses[k], 'model_id': k,
-                         'iters': model._iters, 'best_val_loss': best_['val_loss'],
-                         **configs[k]}
-                         for k, model, in models.items()]
-            history.set(history_)
-            if len(models) == 1:
-                break
-        return True
 
     def fit(self, verbose=True):
         """
@@ -176,14 +178,6 @@ class Hyperband:
         -------
         """
         client = distributed.get_client()
-        R, eta = self.R, self.eta
-        s_max = math.floor(math.log(self.R, self.eta))
-        B = (s_max + 1) * self.R
-        kwargs = []
-        for s in reversed(range(s_max + 1)):
-            n = math.ceil(B/R * eta**s / (s+1))
-            r = R * eta ** -s
-            kwargs += [{'s': s, 'n': n, 'r': r}]
 
         variables = {'val_loss': np.inf, 'model': None, 'config': None,
                      'classes': self.classes.tolist(), 'eta': self.eta,
@@ -197,15 +191,26 @@ class Hyperband:
             client.unpublish_dataset('_dask_data')
         client.publish_dataset(_dask_data=self.data)
 
-        #  futures = [client.submit(self._successive_halving, self.params, eta=eta,
-                                 #  **kwarg, shared=shared, verbose=verbose)
-                   #  for kwarg in kwargs]
-        #  client.gather(futures)
-        futures = [self._successive_halving(self.params, eta=eta,
-                                 **kwarg, shared=shared, verbose=verbose)
-                   for kwarg in kwargs]
+        # now start hyperband
+        R, eta = self.R, self.eta
+        s_max = math.floor(math.log(self.R, self.eta))
+        B = (s_max + 1) * self.R
+        kwargs = []
+        for s in reversed(range(s_max + 1)):
+            n = math.ceil(B/R * eta**s / (s+1))
+            r = R * eta ** -s
+            kwargs += [{'s': s, 'n': n, 'r': r, 'shared': shared,
+                        '_prefix': f's={s}', 'eta': eta, 'verbose': verbose}]
 
-        #  client.gather(futures)
-        self.history = shared['history'].get()
+        #  futures = [client.submit(_successive_halving, self.params,
+                                 #  self.model, **kwarg)
+                   #  for kwarg in kwargs]
+        #  history = sum(client.gather(futures), [])
+
+        futures = [_successive_halving(self.params, self.model, **kwarg)
+                   for kwarg in kwargs]
+        history = sum(futures, [])
+
+        self.history = history
 
         return tuple([shared[k].get() for k in ['val_loss', 'config']])
