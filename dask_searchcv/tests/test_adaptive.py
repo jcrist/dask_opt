9b5a864bb3f424ee718d6ff79013485007be045c
Scott Sievert <github@stsievert.com>
2018-06-03 11:54:09 -0500

    TST: include test with scipy.stats dists

--- dask_searchcv/tests/test_adaptive.py
+++ dask_searchcv/tests/test_adaptive.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pytest
 import distributed
 from pprint import pprint
+import scipy.stats as stats
 
 
 class TestFunction:
@@ -65,6 +66,7 @@ def test_hyperband_test_model():
     assert (df.test_score == df.param_value).all()  # more of a TestFunction test
     assert alg.best_params_['value'] == alg.best_estimator_.value
     assert alg.best_params_['value'] == values.max()
+    assert alg.cv_results_['test_score'][alg.best_index_] == values.max()
 
 
 def test_info():
@@ -96,15 +98,35 @@ def test_info():
                             'bracket_iter': 1.0,
                             'num_models': 3.0,
                             'partial_fit_iters': 4.0}],
-              'total_iters': 63.0,
+              'num_partial_fit_calls': 63.0,
               'num_models': 17,
               'num_cv_splits': 1}
     assert info == expect
-    assert info['total_iters'] == sum(b['num_models'] * b['partial_fit_iters']
-                                      for b in expect['brackets'])
+    assert info['num_partial_fit_calls'] == sum(b['num_models'] * b['partial_fit_iters']
+                                                for b in expect['brackets'])
     assert info['num_models'] == sum(b['num_models'] for b in expect['brackets']
                                      if b['bracket_iter'] == 0)
     assert expect['num_cv_splits'] == 1  # TODO: change this!
+
+
+def test_hyperband_with_distributions():
+    client = _get_client()
+    X, y = make_classification(n_samples=20, n_features=20, chunks=20)
+    model = TestFunction()
+    max_iter = 81
+
+    values = stats.uniform(0, 1)
+    values.random_state = np.random.RandomState(42)
+    params = {'value': values}
+    with pytest.warns(UserWarning, match='model has no attribute warm_start'):
+        alg = Hyperband(model, params, max_iter=max_iter)
+
+    alg.fit(X, y)
+    similar_to_test = stats.uniform(0, 1)
+    similar_to_test = tested_values.rvs(alg.info()['num_models'])
+
+    assert len(alg.cv_results_['param_value']) == len(similar_to_test)
+    assert alg.cv_results_['test_score'].max() > 0.97
 
 
 def test_top_k(k=2):
