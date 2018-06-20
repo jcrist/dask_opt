c76ab50d25f912399ea311aa0cb9d54ce2b1d7ee
Scott Sievert <github@stsievert.com>
2018-06-19 22:41:40 -0500

    ENH: add cross_validation_score

--- dask_searchcv/__init__.py
+++ dask_searchcv/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from .model_selection import GridSearchCV, RandomizedSearchCV
+from .model_selection import cross_validatation_score
 from .adaptive import Hyperband
 
 from ._version import get_versions
